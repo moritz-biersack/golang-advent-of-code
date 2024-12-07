2baec00cffd38aa65e55b57f48f9a767b34d2793
Moritz Biersack <moritz@app-peak.com>
2024-12-07 10:39:13 +0100

    Update day05 to just use sorting

--- day05/main.go
+++ day05/main.go
@@ -44,34 +44,26 @@ func main() {
 	}
 }
 
+func getSortFunc(rules map[int][]int) func(a, b int) int {
+	return func(a, b int) int {
+		rules, included := rules[b]
+		if included && slices.Contains(rules, a) {
+			return 1
+		}
+		return -1
+	}
+}
+
 func part1(input string) int {
 	instructions := strings.Split(input, "\n\n")
 	orderings := strings.Split(instructions[0], "\n")
 	updates := strings.Split(instructions[1], "\n")
 	rulesPerPage := getRulesPerPage(orderings)
+	sortFunc := getSortFunc(rulesPerPage)
 	sum := 0
 	for _, update := range updates {
-		allGood := true
 		pageNumbers := getPageNumbers(update)
-		for i, page := range pageNumbers {
-			good := true
-			pageRules, included := rulesPerPage[page]
-			if !included {
-				continue
-			}
-			for _, r := range pageRules {
-				rIndex := slices.Index(pageNumbers, r)
-				if rIndex < i && rIndex != -1 {
-					good = false
-					break
-				}
-			}
-			if !good {
-				allGood = false
-				break
-			}
-		}
-		if allGood {
+		if slices.IsSortedFunc(pageNumbers, sortFunc) {
 			middle := len(pageNumbers) / 2
 			sum += pageNumbers[middle]
 		}
@@ -110,113 +102,20 @@ func getPageNumbers(update string) []int {
 	return pageNumbers
 }
 
-func getPageNumbersLinked(update string) LinkedList {
-	pageNumberStrings := strings.Split(update, ",")
-	var list LinkedList
-	
-
-	for _, page := range pageNumberStrings {
-		pageInt, err := strconv.Atoi(page)
-		if err != nil {
-			panic("Error converting update")
-		}
-		list.Add(pageInt)
-	}
-
-	return list
-}
-
-type Node struct {
-    value int
-    next  *Node
-}
-
-type LinkedList struct {
-    head *Node
-	len int
-}
-
-func (l *LinkedList) Add(n int) {
-	l.len++
-	newNode := &Node{value: n}
-	if l.head == nil {
-		l.head = newNode
-		return
-	}
-	current := l.head
-	for current.next != nil {
-		current = current.next
-	}
-	current.next = newNode
-}
-
-func (l *LinkedList) Get(i int) *Node {
-	current := l.head
-	for range i {
-		if current.next == nil {
-			panic("Out of index")
-		}
-		current = current.next
-	}
-	return current
-}
-
-func (l *LinkedList) Print() {
-	current := l.head
-	fmt.Print(current.value, "|")
-	for current.next != nil {
-		current = current.next
-		fmt.Print(current.value, "|")
-	}
-	fmt.Println()
-}
-
 func part2(input string) int {
 	instructions := strings.Split(input, "\n\n")
 	orderings := strings.Split(instructions[0], "\n")
 	updates := strings.Split(instructions[1], "\n")
 	rulesPerPage := getRulesPerPage(orderings)
+	sortFunc := getSortFunc(rulesPerPage)
 	sum := 0
 	for _, update := range updates {
-		allGood := true
-		pageNumbers := getPageNumbersLinked(update)
-		for i:=1; i<pageNumbers.len; i++ {
-			page := pageNumbers.Get(i)
-			pageRules, included := rulesPerPage[page.value]
-			if !included {
-				continue
-			}
-			current := pageNumbers.head
-			var prev *Node
-			for j:=0; j<i; j++ {
-				if slices.Contains(pageRules, current.value) {
-					allGood = false
-					if prev == nil {
-						pageNumbers.head = current.next
-					} else {
-						prev.next = current.next
-						prev = prev.next
-					}
-					temp := page.next
-					page.next = current
-					current.next = temp
-					if prev == nil {
-					current = pageNumbers.head
-					} else {
-						current = prev.next
-					}
-					i = j
-				} else {
-					prev = current
-					current = current.next
-				}
-			}
+		pageNumbers := getPageNumbers(update)
+		if slices.IsSortedFunc(pageNumbers, sortFunc) {
+			continue
 		}
-		if !allGood {
-			middle := pageNumbers.len / 2
-			sum += pageNumbers.Get(middle).value
-		}
-
+		slices.SortFunc(pageNumbers, sortFunc)
+		sum += pageNumbers[len(pageNumbers)/2]
 	}
 	
 	return sum
